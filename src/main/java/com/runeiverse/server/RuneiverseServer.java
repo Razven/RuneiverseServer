c868443515cab5cfd0910eab51edc57ff60e8613
Razvan Bangu <raztor2002@hotmail.com>
2014-02-22 13:30:47 -0500

    Raz - pushing starting project and libraries

---
+++ src/main/java/com/runeiverse/server/RuneiverseServer.java
@@ -0,0 +1,11 @@
+package com.runeiverse.server;
+
+/**
+ * Created by Razvan on 22/02/14.
+ */
+public class RuneiverseServer {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/runeiverse/server/tests/RuneiverseServerTest.java
@@ -0,0 +1,7 @@
+package com.runeiverse.server.tests;
+
+/**
+ * Created by Razvan on 22/02/14.
+ */
+public class RuneiverseServerTest {
+}
